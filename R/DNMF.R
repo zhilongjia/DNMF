991d16655fba9dfece99406485a0e2cd2a3fad8b
Zhilong <zhilongjia@gmail.com>
2015-01-08 10:33:38 +0000

    add the label to the returned object of DNMF.

--- R/DNMF.R
+++ R/DNMF.R
@@ -93,5 +93,5 @@ while (final > tol && count <= maxIter) {
     	#plot(obj_stack, type="l", xlab="Times of iteration", ylab="Convergence")
     	heatmap(H)
     }
-    list(W=W, H=H, delta=delta, gamma=gamma, count=count, final=final, obj_stack=obj_stack, r=r)
+    list(W=W, H=H, trainlabel=trainlabel, delta=delta, gamma=gamma, count=count, final=final, obj_stack=obj_stack, r=r)
 }
