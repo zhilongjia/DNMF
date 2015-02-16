4c5d81f29378b94367e77d3c67a9a24f5dde01ef
Zhilong Jia <zhilongjia@gmail.com>
2015-02-16 17:36:20 +0000

    update title, description and an url inrnk function.

--- R/rnk.R
+++ R/rnk.R
@@ -1,10 +1,10 @@
 #' write a rnk file from matrix W.
 #' 
 #' write a rnk file from matrix W in a returned object of function \code{DNMF}.
-#' The rnk format is referred \href{http://www.broadinstitute.org/cancer/software/gsea/wiki/index.php/Data_formats#RNK:_Ranked_list_file_format_.28.2A.rnk.29}{RNK}
+#' The rnk format is referred \href{http://www.broadinstitute.org/cancer/software/gsea/wiki/index.php/Data_formats}{RNK}
 #' 
-#' @param object, a returned object of function \code{DNMF}
-#' @param fn, the output filename
+#' @param object a returned object of function \code{DNMF}
+#' @param fn the output filename
 #' @export
 #' @examples
 #' \dontrun{
