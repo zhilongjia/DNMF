46b6be454e48f5e686b43fbfedb9332505d3b3c2
Zhilong Jia <zhilongjia@gmail.com>
2015-05-15 22:20:31 +0100

    multi-class for rnk

--- R/rnk.R
+++ R/rnk.R
@@ -5,11 +5,29 @@
 #' 
 #' @param object a returned object of function \code{DNMF}
 #' @param fn the output filename. Default is "./tmp.rnk"
+#' @param type type o2m (Default) or o2o. to compare with multi sample labels. 
+#' o2m means one Vs others, while o2o means one Vs another one.
 #' @export
 #' @examples
 #' \dontrun{
-#' rnk(dnmf_result, fn="./tmp.rnk")
+#' rnk(dnmf_result, fn="tmp.rnk")
 #' }
-rnk <- function (object, fn="./tmp.rnk"){
-    write.table(object$rnk, fn, sep="\t", quote=FALSE, col.names=FALSE, row.names=TRUE)
+rnk <- function (object, fn="./tmp.rnk", type="o2m"){
+    
+    type <- match.arg(type, c("o2m", "o2o"))
+    if (object$r==2) {
+        write.table(object$rnk, fn, sep="\t", quote=FALSE, col.names=FALSE, row.names=TRUE)
+    } else if (type=="o2o"){
+        for (i in 1:(r-1)) {
+            for (j in (i+1):r) {
+                write.table(object$W[,j]-object$W[,i], paste0(dirname(fn), "/", j, "_", i, "_", basename(fn)), sep="\t", quote=FALSE, col.names=FALSE, row.names=TRUE)
+            }
+        }
+    } else if (type=="o2m") {
+        for (i in 1:r) {
+            write.table(object$W[,i]-rowMeans(object$W[,-i]), paste0(dirname(fn), "/", i, "_", basename(fn)), sep="\t", quote=FALSE, col.names=FALSE, row.names=TRUE)
+        }
+    }
 }
+
+
